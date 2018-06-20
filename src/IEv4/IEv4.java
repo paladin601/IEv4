477fdb2a49d6b2b5a0661887777562c4f49bcacd
paladin601 <leonardo3103@gmail.com>
2018-06-20 10:35:04 -0400

    first commit

---
+++ src/IEv4/IEv4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IEv4;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class IEv4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+                   new IEv4UI().setVisible(true);
+               });
+    }
+    
+}
