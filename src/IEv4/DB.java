5e34c354132b3ce5063be4bddb599ee270110bbc
adrianjmejias <adrianjmejias@gmail.com>
2018-06-24 01:10:31 -0400

    cambios al algoritmo

--- src/IEv4/DB.java
+++ src/IEv4/DB.java
@@ -4,153 +4,292 @@
  * and open the template in the editor.
  */
 package IEv4;
+
+import static IEv4.Imagensish.EUDistance;
+import java.awt.Color;
 import java.util.*;
 import static org.bytedeco.javacpp.opencv_core.hconcat;
 import static org.bytedeco.javacpp.opencv_core.vconcat;
 import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_core.Point2d;
 import org.bytedeco.javacpp.opencv_core.Rect;
-import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameUtils;
 
 /**
  *
  * @author Mipto Laptop 1
  */
-public class DB {
-
- 
-    public int MinTolerance = 30, MinLocalTolerance = 40;
-    Imagensish[] FramesUsed, FramesUsedHD, TiledImage;
+public class DB
+{
+    public int faltan;
+    Imagensish[] FramesUsed, FramesUsedHD;
     public ArrayList<ArrayList<Imagensish>> Pending;
     public HashSet<Integer> CheckedIndex;
-    public ArrayList<Integer> FramesToMatch;
-    public Point2d[] TileInits;
+    public HashMap<Integer, ArrayList<Imagensish>> OrderedTiles;
     public Mat Img;
-    public boolean CheckingOld=false;
+    public boolean CheckingOld = false;
     private int size;
-//    public ArrayList<Frame> FramesToCheck;    
-//    public ArrayList<Integer> FramesToCheckIndex;
-    
+    private boolean status = false;
 
-    
-       public DB(Mat Img) {
+    public DB(Mat Img)
+    {
         size = IEv4UI._gridX * IEv4UI._gridY;
-           
+        faltan = size;
         FramesUsed = new Imagensish[size];
         FramesUsedHD = new Imagensish[size];
-        TiledImage = new Imagensish[size];
-        TileInits = new Point2d[size];
         CheckedIndex = new HashSet<>();
-        FramesToMatch = new ArrayList<>();
+        OrderedTiles = new HashMap<>();
         Pending = new ArrayList<>();
-           for (int i = 0; i < 4; i++) {
-               Pending.add(new ArrayList<>());
-           }
-        
+        for (int i = 0; i < 4; i++)
+        {
+            Pending.add(new ArrayList<>());
+        }
+
         this.Img = Img;
     }
-    
-    public void TileImage(){
-        for (int yy = 0, initY = 0; yy < IEv4UI._gridY; yy++, initY+= IEv4UI._gridHeight) {
-            for (int xx = 0, initX = 0; xx < IEv4UI._gridX; xx++, initX+= IEv4UI._gridWidth) {
-                int index = yy * IEv4UI._gridX+ xx;
-                this.TileInits[index] = new Point2d(initX, initY);
-                this.TiledImage[index] = new Imagensish(new Mat(this.Img, new Rect(initX, initY, IEv4UI._ExWidth, IEv4UI._ExHeigth)), -1);
-                this.FramesToMatch.add(index);
-                //this.FramesUsed[(index+1)%FramesUsed.length] = this.TiledImage[index];
+
+    public void TileImage()
+    {
+
+        ArrayList<Imagensish> aux;
+        for (int yy = 0, initY = 0; yy < IEv4UI._gridY; yy++, initY += IEv4UI._gridHeight)
+        {
+            for (int xx = 0, initX = 0; xx < IEv4UI._gridX; xx++, initX += IEv4UI._gridWidth)
+            {
+
+                int index = yy * IEv4UI._gridX + xx;
+                Imagensish Piece = new Imagensish(new Mat(this.Img, new Rect(initX, initY, IEv4UI._ExWidth, IEv4UI._ExHeigth)), index);
+                int Magnitude = Piece.getMagnitude();
+
+                if (OrderedTiles.containsKey(Magnitude))
+                {
+                    aux = OrderedTiles.get(Magnitude);
+                } else
+                {
+                    aux = new ArrayList<>();
+                    OrderedTiles.put(Integer.valueOf(Magnitude), aux);
+                }
+                aux.add(Piece);
             }
         }
     }
 
-    private int stride = (int)( Math.random() * IEv4UI._numFrames)%IEv4UI._numFrames;
+    private int stride = (int) (Math.random() * IEv4UI._numFrames) % IEv4UI._numFrames;
     private int nextFrame = 0;
-    public int FindFrame(){
-        do{
-            nextFrame = (nextFrame+stride)%IEv4UI._numFrames;
-            if(this.CheckedIndex.add(nextFrame)){
-                return nextFrame;
-            }
-            else
+
+    public int FindFrame()
+    {
+        if(status && this.CheckedIndex.add(nextFrame+1))
+            return ++nextFrame;
+        
+        do
+        {
+            nextFrame = (nextFrame + stride) % IEv4UI._numFrames;
+            if (this.CheckedIndex.add(nextFrame))
             {
-                stride = (int)( Math.random() * IEv4UI._numFrames)%IEv4UI._numFrames;
+                return nextFrame;
+            } else
+            {
+                stride = (int) (Math.random() * IEv4UI._numFrames) % IEv4UI._numFrames;
             }
-        }while(true);
+        } while (true);
     }
-    
-    public void MatchTiles(){
-        while(FramesToMatch.size() > 0){
+
+    public void MatchTiles()
+    {
+        while (OrderedTiles.size() > 0)
+        {
             int frame = FindFrame();
             Imagensish Comp = null;
             Mat grabbedHD = null, lowPoly = null;
-            
-            try {
+
+            try
+            {
                 IEv4UI._grabber.setFrameNumber(frame);
                 grabbedHD = Java2DFrameUtils.toMat(IEv4UI._grabber.grab());
-                     
-            } catch (Exception e) {
+
+            } catch (Exception e)
+            {
                 e.printStackTrace();
-                System.out.println(e.getMessage()+ "error al agarrar frame");
+                System.out.println(e.getMessage() + "error al agarrar frame");
             }
             lowPoly = IEv4UI.resize(grabbedHD, IEv4UI._gridWidth);
-            Comp = new Imagensish(lowPoly, frame);
 
-            
-            for (int ii = 0; ii < FramesToMatch.size(); ii++) {
-                int index = FramesToMatch.get(ii);
-                if(Imagensish.AlikeImgs(TiledImage[index], Comp)){
-                    this.FramesToMatch.remove(ii);
-                    this.FramesUsed[index] = Comp;
-                    //this.FramesUsedHD[index] = new Imagensish(grabbedHD, frame);
-                    ii = FramesToMatch.size();
-                    IEv4UI.Msg.setText("Faltan "+ ii + "Frames");
+            Comp = new Imagensish(lowPoly, frame);
+            int CompMag = Comp.getMagnitude();
+            System.out.println("inicio de el frame " + frame);
+            if (OrderedTiles.containsKey(CompMag))
+            {
+                System.out.println("Posible Candidato frame----------------------------------");
+                ArrayList<Imagensish> ComparisonList = OrderedTiles.get(CompMag);
+                status = false;
+                int maxLikeliness = -1;
+                for (int ii = 0; ii < ComparisonList.size() && !status; ii++)
+                {
+                    int likelyness = AlikeImgs(ComparisonList.get(ii), Comp);
+                    status = likelyness >= IEv4UI._locality;
+                    if (status)
+                    {
+                        this.FramesUsed[ComparisonList.get(ii).getFrameNumber()] = Comp;
+
+                        ComparisonList.remove(ii);
+                        int imgLeft = ComparisonList.size();
+                        if (imgLeft == 0)
+                        {
+                            OrderedTiles.remove(CompMag);
+                        }
+                        System.out.println(--size+") Faltan " + imgLeft + "Frames y " + OrderedTiles.size()+"pedazos");
+                    }
+                    else
+                    {
+                        System.out.println("rejected");
+                        maxLikeliness = Math.max(maxLikeliness, likelyness);
+
+                    } 
+                }//End For
+                if(!status && maxLikeliness >= 0)
+                {
+                    System.out.println("Para despues");
+                    Pending.get(maxLikeliness).add(Comp);           
                 }
-            }
-            System.out.println("visto el frame "+frame);
-            if((this.CheckedIndex.size()+1)%IEv4UI._change == 0) LowerTheLevel();
-            
+
+                if ((this.CheckedIndex.size() + 1) % IEv4UI._change == 0)
+                {
+                    LowerTheLevel();
+                }
+                System.out.println("Fin Posible candidato frame -----------------------------");
+            }//End Tile Matching
+
+            System.out.println("fin de el frame " + frame);
+
         }
     }
 
-    public void LowerTheLevel(){
+    public void LowerTheLevel()
+    {
+        System.out.println("Bajando el nivel");
         IEv4UI._locality--;
+        if (IEv4UI._locality >= 0)
+        {
+            ArrayList<Imagensish> viejas = Pending.remove(IEv4UI._locality);
+            for (int ii = 0; ii < viejas.size(); ii++)
+            {
+                Imagensish Comp = viejas.get(ii);
+                int CompMag = Comp.getMagnitude();
+
+                if (OrderedTiles.containsKey(CompMag))
+                {
+                    System.out.println("Posible Viejo Candidato --------------------------------------");
+                    ArrayList<Imagensish> ComparisonList = OrderedTiles.get(CompMag);
+
+                    for (int jj = 0; jj < ComparisonList.size(); jj++)
+                    {
+                        int likelyness = AlikeImgs(ComparisonList.get(jj), Comp);
+
+                        if (likelyness >= IEv4UI._locality)
+                        {
+                            this.FramesUsed[ComparisonList.get(jj).getFrameNumber()] = Comp;
+                            ComparisonList.remove(jj);
+                            int imgLeft = ComparisonList.size();
+                            if (imgLeft == 0)
+                            {
+                                OrderedTiles.remove(CompMag);
+                            }
+                            System.out.println(--size+")El viejo lo logró Faltan " + imgLeft + "Frames");
+                            break;
+                        }
+                    }//End For
+                    System.out.println("Fin Posible Viejo Candidato -------------------------------------");
+
+                }//End Tile Matching
+            }
+        }
     }
-    public Mat GenerateMosaic(){
+
+    public Mat GenerateMosaic()
+    {
         TileImage();
         MatchTiles();
         System.out.println("Generating mosaic");
-        Mat aux=null,aux1=null;
-        int i=0;
-       for(int jj=0;jj<this.FramesUsed.length ;jj++){
-            if((jj%IEv4UI._gridX)==0 ){
-                if(jj!=0){
-                    if(i==0){
-                        aux1=aux.clone();
+        Mat aux = null, aux1 = null;
+        int i = 0;
+        for (int jj = 0; jj < this.FramesUsed.length; jj++)
+        {
+            if ((jj % IEv4UI._gridX) == 0)
+            {
+                if (jj != 0)
+                {
+                    if (i == 0)
+                    {
+                        aux1 = aux.clone();
                         i++;
-                    }else{
+                    } else
+                    {
                         vconcat(aux1, aux, aux1);
-                    } 
+                    }
                 }
                 aux = this.FramesUsed[jj].getImg().clone();
-            }else{
-               // if((jj%IEv3UI._gridX)!=0){
-                    hconcat(aux,FramesUsed[jj].getImg(),aux);
+            } else
+            {
+                // if((jj%IEv3UI._gridX)!=0){
+                hconcat(aux, FramesUsed[jj].getImg(), aux);
                 //}
-            }          
+            }
         }
-       vconcat(aux1, aux, aux1);
+        vconcat(aux1, aux, aux1);
         return aux1;
     }
-    
-    static double  f(double t){
-        final double es3 = Math.pow(6/29,3)
-       ,es2 = Math.pow(6/29,2);
-        
-         if(t > es3){
-             t = Math.cbrt(t);
-         }else{
-             t = (t/(3 * es2)) + (4/29);
-         }
-         return t;
-     }
+
+    static double f(double t)
+    {
+        final double es3 = Math.pow(6 / 29, 3), es2 = Math.pow(6 / 29, 2);
+
+        if (t > es3)
+        {
+            t = Math.cbrt(t);
+        } else
+        {
+            t = (t / (3 * es2)) + (4 / 29);
+        }
+        return t;
+    }
+
+    public static int AlikeImgs(Imagensish A, Imagensish B)
+    {
+        if (IEv4UI._locality < 0)
+        {
+            return Integer.MAX_VALUE;
+        }
+        Color[] promA = A.getProm(), promB = B.getProm();
+        int local = 0;
+        double dist;
+        if (IEv4UI._euclidianComp)
+        {
+            dist = EUDistance(promA[0], promB[0]);
+            if (dist > IEv4UI._minTolerance)
+            {
+                System.out.println(promA[0] + "vs" + promB[0] + " = " + dist);
+                return Integer.MIN_VALUE;
+            } else
+            {
+                if (IEv4UI._locality == 0)
+                {
+                    System.out.println("Averageing");
+                    return 0;
+                }
+            }
+            for (int ii = 1; ii < promA.length && local < IEv4UI._locality; ii++)
+            {
+                dist = EUDistance(promA[ii], promB[ii]);
+                if (dist <= IEv4UI._minLocalTolerance)
+                {
+                    local++;
+                }
+            }
+        } else
+        {
+
+        }
+        return local;
+    }
+
 }
--- src/IEv4/Imagensish.java
+++ src/IEv4/Imagensish.java
@@ -10,223 +10,213 @@ import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.Rect;
-import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Java2DFrameUtils;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacpp.opencv_imgproc.*;
+
 /**
  *
  * @author Mipto Laptop 1
  */
-public class Imagensish {
+public class Imagensish
+{
+
+    private int Magnitude = 0;
+
     private Color[] Prom;
     private Mat Img;
     private int FrameNumber;
 
     //Constructores----------------------------------------------------------------------------------
-    public Imagensish(Mat Img, int FrameNumber) {
+    public Imagensish(Mat Img, int FrameNumber)
+    {
         this.Img = Img;
         this.FrameNumber = FrameNumber;
         Prom = new Color[5];
-        
+
         MeasureImg(Java2DFrameUtils.toBufferedImage(Img));
     }
-    public Imagensish(BufferedImage Img, int FrameNumber) {
+
+    public Imagensish(BufferedImage Img, int FrameNumber)
+    {
         this.Img = Java2DFrameUtils.toMat(Img);
         this.FrameNumber = FrameNumber;
         Prom = new Color[5];
-        
+
         MeasureImg(Img);
     }
-    public Imagensish(Frame Img, int FrameNumber) {
+
+    public Imagensish(Frame Img, int FrameNumber)
+    {
         this.Img = Java2DFrameUtils.toMat(Img);
         this.FrameNumber = FrameNumber;
         Prom = new Color[5];
-        
+
         MeasureImg(Java2DFrameUtils.toBufferedImage(Img));
     }
-    
+
     //Done
-    public final void MeasureImg(BufferedImage In){
-        
-        Prom[1] = MeasureROI(In, new Rect(0, 0, IEv4UI._gridHalfWidth-1 ,IEv4UI._gridHalfHeight-1));
-        Prom[2] = MeasureROI(In, new Rect(IEv4UI._gridHalfWidth, 0, IEv4UI._gridHalfWidth-1, IEv4UI._gridHalfHeight-1));
-        Prom[3] = MeasureROI(In, new Rect(0, IEv4UI._gridHalfHeight, IEv4UI._gridHalfWidth-1, IEv4UI._gridHalfHeight-1));
-        Prom[4] = MeasureROI(In, new Rect(IEv4UI._gridHalfWidth, IEv4UI._gridHalfHeight, IEv4UI._gridHalfWidth-1, IEv4UI._gridHalfHeight-1));
-        
+    public final void MeasureImg(BufferedImage In)
+    {
+
+        Prom[1] = MeasureROI(In, new Rect(0, 0, IEv4UI._gridHalfWidth - 1, IEv4UI._gridHalfHeight - 1));
+        Prom[2] = MeasureROI(In, new Rect(IEv4UI._gridHalfWidth, 0, IEv4UI._gridHalfWidth - 1, IEv4UI._gridHalfHeight - 1));
+        Prom[3] = MeasureROI(In, new Rect(0, IEv4UI._gridHalfHeight, IEv4UI._gridHalfWidth - 1, IEv4UI._gridHalfHeight - 1));
+        Prom[4] = MeasureROI(In, new Rect(IEv4UI._gridHalfWidth, IEv4UI._gridHalfHeight, IEv4UI._gridHalfWidth - 1, IEv4UI._gridHalfHeight - 1));
+
         float red = 0, green = 0, blue = 0;
-        for (int ii = 1; ii < Prom.length; ii++) {
-            red   += Prom[ii].getRed();
+        for (int ii = 1; ii < Prom.length; ii++)
+        {
+            red += Prom[ii].getRed();
             green += Prom[ii].getGreen();
-            blue  += Prom[ii].getBlue();
+            blue += Prom[ii].getBlue();
         }
-        red   /= 4;        
+        red /= 4;
         green /= 4;
-        blue  /= 4;
-        
+        blue /= 4;
+
         Prom[0] = new Color(Clamp(red), Clamp(green), Clamp(blue));
+        Magnitude = (int) EUDistance(Color.BLACK, Prom[0]);
     }
+
     //Done
-    public Color MeasureROI(BufferedImage In, Rect ROI){
-        
+    public Color MeasureROI(BufferedImage In, Rect ROI)
+    {
+
         float red = 0, green = 0, blue = 0;
-        int limy= ROI.y() + ROI.height();        
-        int limx= ROI.x() + ROI.width();
-        if(limx> In.getWidth()|| limy > In.getHeight()) System.out.println("la tas cagado serio");
-        for (int yy = ROI.y(); yy < limy; yy++) {
-            for (int xx = ROI.x(); xx < limx; xx++) {
+        int limy = ROI.y() + ROI.height();
+        int limx = ROI.x() + ROI.width();
+        if (limx > In.getWidth() || limy > In.getHeight())
+        {
+            System.out.println("la tas cagado serio");
+        }
+        for (int yy = ROI.y(); yy < limy; yy++)
+        {
+            for (int xx = ROI.x(); xx < limx; xx++)
+            {
                 Color aux = new Color(In.getRGB(xx, yy));
-                
-                red   += aux.getRed();
+
+                red += aux.getRed();
                 green += aux.getGreen();
-                blue  += aux.getBlue();
+                blue += aux.getBlue();
             }
         }
         int size = ROI.height() * ROI.width();
-        red   /= size;        
+        red /= size;
         green /= size;
-        blue  /= size;
-        
+        blue /= size;
+
         return new Color(Clamp(red), Clamp(green), Clamp(blue));
     }
-    public static int Clamp(Number in){
+
+    public static int Clamp(Number in)
+    {
         return Math.max(0, Math.min(255, in.intValue()));
     }
-    
-    public static boolean AlikeImgs(Imagensish A, Imagensish B){
-        if(IEv4UI._locality < 0) return true;
-        Color[] promA = A.getProm(), promB = B.getProm();
-        int local = 0;
-        double dist;
-        if(IEv4UI._euclidianComp){
-            dist = EUDistance(promA[0], promB[0]);
-            if( dist > IEv4UI._minTolerance){
-                System.out.println(promA[0] +"vs"+ promB[0]+" = "+dist);
-                return false;
-            }else{
-                System.out.println("good");
-                if(IEv4UI._locality == 0) return true;
-            }
-            for (int ii = 1; ii < promA.length && local < IEv4UI._locality; ii++){
-                dist = EUDistance(promA[ii], promB[ii]);
-                if(dist > IEv4UI._minLocalTolerance) {
-                }else{
-                    local++;
-                }
-            }
-        }
-        else
-        {
-            dist = LABDistance(promA[0], promB[0]);
-            if( dist > IEv4UI._minTolerance){
-                System.out.println(promA[0] +"vs"+ promB[0]+" = "+dist);
-                return false;
-            }else{
-                System.out.println("good");
-                if(IEv4UI._locality == 0) return true;
-            }
-            for (int ii = 1; ii < promA.length && local < IEv4UI._locality; ii++){
-                dist = LABDistance(promA[ii], promB[ii]);
-                if(dist > IEv4UI._minLocalTolerance) {
-                }else{
-                    local++;
-                }
-            }
-        }
-        return local >= IEv4UI._locality;
-    }
-    
-    
-    public static double EUDistance(Color A, Color B){
+
+    public static double EUDistance(Color A, Color B)
+    {
         double red_dist = A.getRed() - B.getRed();
         double green_dist = A.getGreen() - B.getGreen();
         double blue_dist = A.getBlue() - B.getBlue();
-        double re = Math.sqrt(red_dist*red_dist + green_dist*green_dist + blue_dist*blue_dist);
+        double re = Math.sqrt(red_dist * red_dist + green_dist * green_dist + blue_dist * blue_dist);
         return re;
     }
-    
-    public static double LABDistance(Color A, Color B){
-        HashMap<String,Double> A1 = Imagensish.RGBtoLAB(A);
-        HashMap<String,Double> B1 = Imagensish.RGBtoLAB(B);
+
+    public static double LABDistance(Color A, Color B)
+    {
+        HashMap<String, Double> A1 = Imagensish.RGBtoLAB(A);
+        HashMap<String, Double> B1 = Imagensish.RGBtoLAB(B);
         double red_dist = A1.get("L") - B1.get("L");
         double green_dist = A1.get("A") - B1.get("A");
         double blue_dist = A1.get("B") - B1.get("B");
-        return Math.sqrt(red_dist*red_dist + green_dist*green_dist + blue_dist*blue_dist);
+        return Math.sqrt(red_dist * red_dist + green_dist * green_dist + blue_dist * blue_dist);
     }
-    
-    public static HashMap<String,Double> RGBtoLAB(Color in){
-        HashMap<String,Double> re = new HashMap<>();
-        HashMap<String,Double> aux=RGBtoXYZ(in);
-        double l,a,b,x,y,z,xn,yn,zn;
+
+    public static HashMap<String, Double> RGBtoLAB(Color in)
+    {
+        HashMap<String, Double> re = new HashMap<>();
+        HashMap<String, Double> aux = RGBtoXYZ(in);
+        double l, a, b, x, y, z, xn, yn, zn;
         //valores constastes triestimulos 
-        xn=95.057f;
-        yn=100.0f;
-        zn=108.883f;
-        
-        x=aux.get("X");
-        y=aux.get("Y");
-        z=aux.get("Z");
-        
+        xn = 95.057f;
+        yn = 100.0f;
+        zn = 108.883f;
+
+        x = aux.get("X");
+        y = aux.get("Y");
+        z = aux.get("Z");
+
         // aca es x/xn y y/yn y z/zn
-        
-        l=116*f(y/yn) - 16;
-        a=500*(f(x/xn) - f(y/yn));
-        b=200*(f(y/yn) - f(z/zn));
-        
-        re.put("L",l);
-        re.put("A",a);
-        re.put("B",b);
-        
-        
-        return re;          
+        l = 116 * f(y / yn) - 16;
+        a = 500 * (f(x / xn) - f(y / yn));
+        b = 200 * (f(y / yn) - f(z / zn));
+
+        re.put("L", l);
+        re.put("A", a);
+        re.put("B", b);
+
+        return re;
     }
-    
-    public static HashMap<String,Double> RGBtoXYZ(Color in){
-        HashMap<String,Double> re = new HashMap<>(); 
-        double r,g,b;
-        double x,y,z;
+
+    public static HashMap<String, Double> RGBtoXYZ(Color in)
+    {
+        HashMap<String, Double> re = new HashMap<>();
+        double r, g, b;
+        double x, y, z;
         //normalizar
-        r=in.getRed()/255.0f;
-        g=in.getGreen()/255.0f;
-        b=in.getBlue()/255.0f;
-        
-        r = r <= 0.04045 ? r/12.92 : Math.pow((r+0.055)/1.055, 2.4);
-	g = g <= 0.04045 ? g/12.92 : Math.pow((g+0.055)/1.055, 2.4);
-	b = b <= 0.04045 ? b/12.92 : Math.pow((b+0.055)/1.055, 2.4);
-        
+        r = in.getRed() / 255.0f;
+        g = in.getGreen() / 255.0f;
+        b = in.getBlue() / 255.0f;
+
+        r = r <= 0.04045 ? r / 12.92 : Math.pow((r + 0.055) / 1.055, 2.4);
+        g = g <= 0.04045 ? g / 12.92 : Math.pow((g + 0.055) / 1.055, 2.4);
+        b = b <= 0.04045 ? b / 12.92 : Math.pow((b + 0.055) / 1.055, 2.4);
+
         //srgb d65
-        x= r*0.4124564f + g*0.3575761f + b*0.1804375f;
-        y= r*0.2126729f + g*0.7151522f + b*0.0721750f;
-        z= r*0.0193339f + g*0.1191920f + b*0.9503041f;
-        
-        re.put("X",x);
-        re.put("Y",y);
-        re.put("Z",z);       
-        
-        return re;          
+        x = r * 0.4124564f + g * 0.3575761f + b * 0.1804375f;
+        y = r * 0.2126729f + g * 0.7151522f + b * 0.0721750f;
+        z = r * 0.0193339f + g * 0.1191920f + b * 0.9503041f;
+
+        re.put("X", x);
+        re.put("Y", y);
+        re.put("Z", z);
+
+        return re;
 
     }
-    
-    public static double  f(double t){
-        final double es3 = Math.pow(6/29,3)
-       ,es2 = Math.pow(6/29,2);
-        
-         if(t > es3){
-             t = Math.cbrt(t);
-         }else{
-             t = (t/(3 * es2)) + (4/29);
-         }
-         return t;
-     }
-    
+
+    public static double f(double t)
+    {
+        final double es3 = Math.pow(6 / 29, 3), es2 = Math.pow(6 / 29, 2);
+
+        if (t > es3)
+        {
+            t = Math.cbrt(t);
+        } else
+        {
+            t = (t / (3 * es2)) + (4 / 29);
+        }
+        return t;
+    }
+
     //Getters----------------------------------------------------------------------------------
-    public Color[] getProm() {
+    public Color[] getProm()
+    {
         return Prom;
     }
-    public Mat getImg() {
+
+    public Mat getImg()
+    {
         return Img;
     }
-    public int getFrameNumber() {
+
+    public int getFrameNumber()
+    {
         return FrameNumber;
+    }
+
+    public int getMagnitude()
+    {
+        return Magnitude%(int)IEv4UI._minTolerance;
     }
 }
